d4d8be1826808d9a0c1db0b616244721d5eb8764
yaferdln <deleonyafer@gmail.com>
2025-05-24 03:50:03 -0700

    initial commit

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -24,14 +24,14 @@ const services = [
 const SalonMarqueeIcons = () => {
   return (
     <div
-      className="mt-4 bg-slate-900/40 py-6 px-6 overflow-hidden"
+      className="mt-4 bg-slate-900/40 py-6 px-4 md:px-6 overflow-hidden"
       data-aos="zoom-in"
     >
       <Marquee gradient={false} speed={50} autoFill pauseOnHover>
         {services.map((service, index) => (
           <div
             key={index}
-            className="mx-6 flex flex-col items-center justify-center min-w-[100px] text-center"
+            className="mx-4 flex flex-col items-center justify-center min-w-[80px] text-center"
           >
             <service.icon size={40} className="text-slate-400 mb-2" />
             <p className="text-xs md:text-sm text-slate-200  whitespace-nowrap">
