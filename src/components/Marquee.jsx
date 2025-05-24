d2b8df16d9af1b2d493c8f0662e233721e79c171
yaferdln <deleonyafer@gmail.com>
2025-05-24 03:57:31 -0700

    initial commit

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,26 +1,3 @@
-import Marquee from "react-fast-marquee";
-import { FaPalette, FaHandSparkles, FaCut } from "react-icons/fa";
-import {
-  GiFootprint,
-  GiEyelashes,
-  GiBarefoot,
-  GiFingernail,
-} from "react-icons/gi";
-import { MdFaceRetouchingNatural } from "react-icons/md";
-// Define the services with their icons and labels
-import { PiHairDryerFill } from "react-icons/pi";
-const services = [
-  { icon: FaCut, label: "Haircut" },
-  { icon: PiHairDryerFill, label: "Hair Rebond" },
-  { icon: FaPalette, label: "Hair Color" }, // Using FaPaintBrush for Hair Color
-  { icon: FaHandSparkles, label: "Manicure" },
-  { icon: GiFootprint, label: "Pedicure" },
-  { icon: GiBarefoot, label: "Foot Spa" },
-  { icon: GiEyelashes, label: "Eyelash" },
-  { icon: MdFaceRetouchingNatural, label: "Facial" },
-  { icon: GiFingernail, label: "Gel Polish" },
-];
-
 const SalonMarqueeIcons = () => {
   return (
     <div
@@ -31,10 +8,10 @@ const SalonMarqueeIcons = () => {
         {services.map((service, index) => (
           <div
             key={index}
-            className="mx-4 flex flex-col items-center justify-center min-w-[80px] text-center"
+            className="mx-3 flex-shrink-0 flex flex-col items-center justify-center min-w-[70px] sm:min-w-[80px] text-center"
           >
-            <service.icon size={40} className="text-slate-400 mb-2" />
-            <p className="text-xs md:text-sm text-slate-200  whitespace-nowrap">
+            <service.icon size={36} className="text-slate-400 mb-2" />
+            <p className="text-xs md:text-sm text-slate-200 whitespace-nowrap">
               {service.label}
             </p>
           </div>
@@ -43,5 +20,4 @@ const SalonMarqueeIcons = () => {
     </div>
   );
 };
-
 export default SalonMarqueeIcons;
